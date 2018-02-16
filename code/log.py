432bf3cef26a805e081191ac0275fe8b13d537b6
benoitguigal <benoit.guigal@gmail.com>
2018-02-16 16:13:45 +0100

    indent with 4 spaces as per PEP8 style guide

--- code/log.py
+++ code/log.py
@@ -10,10 +10,10 @@ from cStringIO import StringIO
 import config
 
 def err():
-	#exc_info=sys.exc_info()
-	exc_type, exc_obj, exc_tb = sys.exc_info()
-	return "{} : {} line {}".format(str(exc_type),exc_obj,exc_tb.tb_lineno)
-	#return "{}".format(traceback.print_exception(*exc_info))
+    #exc_info=sys.exc_info()
+    exc_type, exc_obj, exc_tb = sys.exc_info()
+    return "{} : {} line {}".format(str(exc_type),exc_obj,exc_tb.tb_lineno)
+    #return "{}".format(traceback.print_exception(*exc_info))
 
 def WHERE( back = 0 ):
     frame = sys._getframe( back + 1 )
@@ -22,69 +22,69 @@ def WHERE( back = 0 ):
                         # frame.f_lineno, frame.f_code.co_name )
 
 class Log(object):
-	def __init__(self,name=None,test=False):
-		self.name=name
-		self.chunk="init"
-		self.test=test
-		self.start=datetime.datetime.now()
-		self.writer=sys.stdout
-		if (self.test==True):
-			self.writer=StringIO()
-			self.level=2
-		else:
-			if ("log" in config.conf["global"].keys()):
-				try:
-					self.dir=config.conf["global"]["log"]["dir"]
-				except:
-					self.dir=""
-				self.file="{}./{}-{}.log".format(self.dir,datetime.datetime.now().isoformat(),self.name)
+    def __init__(self,name=None,test=False):
+        self.name=name
+        self.chunk="init"
+        self.test=test
+        self.start=datetime.datetime.now()
+        self.writer=sys.stdout
+        if (self.test==True):
+            self.writer=StringIO()
+            self.level=2
+        else:
+            if ("log" in config.conf["global"].keys()):
+                try:
+                    self.dir=config.conf["global"]["log"]["dir"]
+                except:
+                    self.dir=""
+                self.file="{}./{}-{}.log".format(self.dir,datetime.datetime.now().isoformat(),self.name)
 
-				try :
-					self.writer=open(self.file,"w+")
-				except:
-					self.writer=sys.stdout
+                try :
+                    self.writer=open(self.file,"w+")
+                except:
+                    self.writer=sys.stdout
 
-			try:
-				self.level=config.conf["global"]["log"]["level"]
-			except:
-				self.level=1
+            try:
+                self.level=config.conf["global"]["log"]["level"]
+            except:
+                self.level=1
 
-		try:
-			self.verbose=config.conf["global"]["log"]["verbose"]
+        try:
+            self.verbose=config.conf["global"]["log"]["verbose"]
 
-		except:
-			self.verbose=False
+        except:
+            self.verbose=False
 
-	def write(self,msg=None,error=None,exit=False,level=1):
-		try:
-			self.writer
-		except:
-			return
-		if (type(self.chunk) ==  int):
-			prefix="chunk "
-		else:
-			prefix=""
+    def write(self,msg=None,error=None,exit=False,level=1):
+        try:
+            self.writer
+        except:
+            return
+        if (type(self.chunk) ==  int):
+            prefix="chunk "
+        else:
+            prefix=""
 
-		if (level<=self.level):
-			t = datetime.datetime.now()
-			d = (t-self.start)
-			if (error != None):
-				if (msg != None):
-					fmsg="{} - {} - {}{} : {} - Ooops: {} - {}".format(t,d,prefix,self.chunk,WHERE(1),msg,error)
-				else:
-					fmsg="{} - {} - {}{} : {} - Ooops: {}".format(t,d,prefix,self.chunk,WHERE(1),error)
-			else:
-				fmsg="{} - {} - {}{} : {} - {}".format(t,d,prefix,self.chunk,WHERE(1),msg)
-			try:
-				if (self.verbose==True):
-					print(fmsg)
-			except:
-				pass
+        if (level<=self.level):
+            t = datetime.datetime.now()
+            d = (t-self.start)
+            if (error != None):
+                if (msg != None):
+                    fmsg="{} - {} - {}{} : {} - Ooops: {} - {}".format(t,d,prefix,self.chunk,WHERE(1),msg,error)
+                else:
+                    fmsg="{} - {} - {}{} : {} - Ooops: {}".format(t,d,prefix,self.chunk,WHERE(1),error)
+            else:
+                fmsg="{} - {} - {}{} : {} - {}".format(t,d,prefix,self.chunk,WHERE(1),msg)
+            try:
+                if (self.verbose==True):
+                    print(fmsg)
+            except:
+                pass
 
-			self.writer.write(fmsg+"\n")
-			self.writer.flush()
-			if (exit):
-				#os._exit(1)
-				sys.exit(fmsg)
-			return fmsg
+            self.writer.write(fmsg+"\n")
+            self.writer.flush()
+            if (exit):
+                #os._exit(1)
+                sys.exit(fmsg)
+            return fmsg
 
