d6668d0f5d275af2639466fed30a47b3626a687e
antoinefa <fabien.antoine@m4x.org>
2018-03-06 10:03:48 +0100

    Validation : separate domain and es_proxy_path

--- code/api.py
+++ code/api.py
@@ -387,13 +387,13 @@ class pushToValidation(Resource):
                         configfile = os.path.join(config.conf["global"]["paths"][
                                                   "validation"], secure_filename(conf + ".json"))
                         dic = {
-                            "prefix": config.conf["global"]["api"]["prefix"],
                             "domain": config.conf["global"]["api"]["domain"],
+                            "es_proxy_path": config.conf["global"]["api"]["es_proxy_path"],
                             "dataset": dataset
                         }
                         props[conf] = replace_dict(cfg[conf], dic)
-                        # with open(configfile, 'w') as outfile:
-                        #     json.dump(props[config],outfile,indent=2)
+                        print conf
+                    print {"dataset": dataset, "status": "to validation", "props": props}
                     return {"dataset": dataset, "status": "to validation", "props": props}
                 except:
                         return api.abort(500, {"dataset": dataset, "status": "error: " + err()})
@@ -416,8 +416,8 @@ class pushToValidation(Resource):
                         configfile = os.path.join(config.conf["global"]["paths"][
                                                   "search"], secure_filename(config + ".json"))
                         dic = {
-                            "prefix": config.conf["global"]["api"]["prefix"],
                             "domain": config.conf["global"]["api"]["domain"],
+                            "es_proxy_path": config.conf["global"]["api"]["es_proxy_path"],
                             "dataset": dataset
                         }
                         props[config] = replace_dict(cfg[config], dic)
